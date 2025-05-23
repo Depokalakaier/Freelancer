6454469065d64a45d7536a7490e8cc60c0483805
edzio <depowskimaksymilian@gmail.com>
2025-05-23 18:33:50 +0200

    dodanie kodu authoryzacji do gradle i zmienienie android manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+
 }
 
 android {
@@ -32,12 +33,21 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp (do logowania zapytań – pomocne przy debugowaniu)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
+    // Jeśli chcesz pracować z JsonObject (z GSON lub Moshi):
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.crashlytics.buildtools)
+    implementation(libs.auth.api.impl)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
